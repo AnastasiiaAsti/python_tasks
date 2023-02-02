872def12eeed89209c0e54e545cf65a29b4c5535
Asti <astistateofmind@gmail.com>
2023-02-02 14:13:25 -0800

    adds one liner

--- simple_multiply.py
+++ simple_multiply.py
@@ -5,3 +5,9 @@ def simple_multiplication(number):
         return number * 8
     else:
         return number * 9
+
+#  option 2
+
+
+def simple_multiplication(number):
+    return number * 9 if number % 2 else number * 8
